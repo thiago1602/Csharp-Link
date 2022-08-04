622df6b6a1e005fb00072dcfb9057fb96e43e402
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-08-03 21:45:18 -0300

    Adicionar arquivos de projeto.

---
+++ RealizandoFiltros/Despesa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RealizandoFiltros
+{
+
+
+	public class Despesa
+	{
+		public string Descricao { get; set; }
+		public decimal Valor { get; set;}
+		public DateTime Vencimento { get; set; }
+
+		
+	}
+}
---
+++ RealizandoFiltros/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RealizandoFiltros
+{
+    class Program
+    {
+        private static List<Despesas> despesas = new List<Despesas>
+        {
+            new Despesas {Descricao = "Internet", Valor = 100, Vencimento = new DateTime(2021, 10, 01)},
+            new Despesas {Descricao = "Celular", Valor = 80, Vencimento = new DateTime(2021, 10, 10)},
+            new Despesas {Descricao = "Aluguel", Valor = 1200, Vencimento = new DateTime(2021, 10, 15)},
+            new Despesas {Descricao = "Contabilidade", Valor = 400, Vencimento = new DateTime(2021, 10, 18)}
+        };
+
+        static void Main(string[] args)
+        {
+            var despesasVencidas = from d in despesas
+                                   where d.Valor < 500 && d.Vencimento <= new DateTime(2021, 10, 30)
+                                   select d;
+            foreach (var d in despesasVencidas)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
